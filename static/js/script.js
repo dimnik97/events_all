726af71a7ee194630b6cf3bb0dbc53b5479d339e
Дмитрий <>
2018-10-22 14:38:43 +0400

    верстка профиля

--- static/js/script.js
+++ static/js/script.js
@@ -1204,4 +1204,18 @@ $(document).ready(function() {
 
         return indexed_array;
     }
+
+    $('.popup_header_menu_down').on('click', function(){
+        $(this).hide();
+        $('.popup_header_menu_up').show();
+        $('.popup_menu_block').show();
+
+    });
+
+    $('.popup_header_menu_up').on('click', function(){
+        $(this).hide();
+        $('.popup_header_menu_down').show();
+        $('.popup_menu_block').hide();
+    });
+
 });
