3d4164e240c186cdf31b7764cde50f3838964576
Дмитрий <>
2018-10-24 10:27:45 +0400

    dump

--- events_/models.py
+++ events_/models.py
@@ -265,7 +265,7 @@ class Event(models.Model):
 class EventLikes(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE, default=True)
     date = models.DateField(null=True, blank=True, default=datetime.date.today)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=True, unique=True)
 
     def __str__(self):
         return "Лайк на событие: " + str(self.event) + " от " + str(self.user.first_name)
