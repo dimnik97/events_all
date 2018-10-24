5cba3142e32435b2f600059bd4e043de255c45c3
Дмитрий <>
2018-10-24 17:34:22 +0400

    Профиль, шапка над событиями, события

--- events_all/settings.py
+++ events_all/settings.py
@@ -143,14 +143,14 @@ if (getpass.getuser() == 'dmitrij'):
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test8',
-        'USER': 'postgreadmin',
-        'PASSWORD': '4321',
-        'HOST': 'localhost',
-        # 'NAME': 'local_2',
-        # 'USER': 'postgres',
-        # 'PASSWORD': '',
-        # 'HOST': host,
+        # 'NAME': 'test8',
+        # 'USER': 'postgreadmin',
+        # 'PASSWORD': '4321',
+        # 'HOST': 'localhost',
+        'NAME': 'local_2',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': host,
         'PORT': '5432',
     }
 }
